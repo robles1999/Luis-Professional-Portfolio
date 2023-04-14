aad6a4756aa8e0163fd3e70abdb1cfdca088e66f
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-04-14 15:56:05 -0400

    updated portfolio with the first project info and link to the website

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-document.querySelector(".img-1").addEventListener('click', () => alert('Website under construction. Please try again later.'))
-document.querySelector(".img-2").addEventListener('click', () => alert('Website under construction. Please try again later.'))
-document.querySelector(".img-3").addEventListener('click', () => alert('Website under construction. Please try again later.'))
-document.querySelector(".img-4").addEventListener('click', () => alert('Website under construction. Please try again later.'))
-document.querySelector(".img-5").addEventListener('click', () => alert('Website under construction. Please try again later.'))
-
+document
+  .querySelector(".img-1")
+  .addEventListener(
+    "click",
+    () =>
+      (window.location.href = "https://egarrisxn.github.io/player-one-reads/")
+  );
+document
+  .querySelector(".img-2")
+  .addEventListener("click", () =>
+    alert("Website under construction. Please try again later.")
+  );
+document
+  .querySelector(".img-3")
+  .addEventListener("click", () =>
+    alert("Website under construction. Please try again later.")
+  );
+document
+  .querySelector(".img-4")
+  .addEventListener("click", () =>
+    alert("Website under construction. Please try again later.")
+  );
+document
+  .querySelector(".img-5")
+  .addEventListener("click", () =>
+    alert("Website under construction. Please try again later.")
+  );
